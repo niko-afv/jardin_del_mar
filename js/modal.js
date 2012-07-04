b456582c791e41230841e0279fc7f9c9ce5114af
Nicolás Fredes <niko.afv@gmail.com>
2012-07-04 01:49:35 -0400

    Commit's Info:
    - Se añadió funcionalidad al item Ubicación del menu, que muestra un mapa.
    - Se agregarón archivos para comenzar a trabajar con la funcionalidad de master plan
    - Se eliminó el item videos del menu y la foto de la vendedora en el footer

--- js/modal.js
+++ js/modal.js
@@ -13,7 +13,16 @@ $(function(){
     if(getParameterByName('page') == 'gallery'){
         $("#main").load("gallery.htm");
     }
+    if(getParameterByName('page') == 'masterplan'){
+        $("#main").load("masterplan.htm");
+    }
     if(getParameterByName('page') == 'plantas'){
         $("#main").load("plantas.htm");
+    }
+    if(getParameterByName('page') == 'tour'){
+        $("#main").load("tour_virtual_jdm/2d.html");
+    }
+    if(getParameterByName('page') == 'map'){
+        $("#main").load("map.htm");
     }
 });
